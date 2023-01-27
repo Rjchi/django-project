10862c640d13cbd8ec70355c905d39ba6f92cfc2
Richi <richardospina18@gmail.com>
2023-01-27 18:17:41 -0500

    Herencia de templates y jinja2

--- myapp/urls.py
+++ myapp/urls.py
@@ -7,6 +7,7 @@ from myapp import views
 # El punto es para referirce a la ruta actual (en este caso es myapp)
 from . import views
 
+#  Aqui tenemos las urls que vamos a utilizar
 urlpatterns = [
     path('', views.index),
     # Con esto decimos que entre en la ruta 'ruta/ruta.com/' y que ejecute una funcion que esta dentro de views
@@ -15,4 +16,7 @@ urlpatterns = [
     # Con esto lo que hacemos es crear una variable que esperamos, y extraerla con la funcion
     # tambien podemos hacerlo con un int ejm: hello/<int:id>
     path('hello/<str:usuario>', views.unaFuncion),
+
+    path('projects/', views.projectos),
+    path('task/', views.tareas)
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,22 @@
-from django.http import HttpResponse
-# Create your views here.
+# Importamos JsonRespon para poder devolver un formato que el navegador pueda
+# entender
+from django.http import HttpResponse, JsonResponse
+# fn + f1 settings (JSON) ---> "editor.wordWarp": "on"
+# Importamos render para renderizar paginas
+from django.shortcuts import render
+
+# Importamos los modelos para hacer consultas a la base de datos
+from .models import tabla_uno_project, modelo_tareas_del_project
+
+# Devolvemos una pagina 404 en caso de un error a traves de una funcion
+# propia de django
+from django.shortcuts import get_object_or_404
+
 # LA CONVENCION RECOMENDADA PARA PYTHON ES Snake Case variable_uno
+# El patron de diseño que generalmente se utiliza en django es el MTV
+# Modelo, Plantilla, Vista
+# La logica de negocio va en la vista(la vista en este caso es similar
+# al controlador del patron MVC) y las vistas serian las plantillas
 
 # 1- Instalar python
 # 2- Crear una carpeta
@@ -37,18 +53,54 @@ from django.http import HttpResponse
 # con su  ORM (las tablas ya traen funciones para insertar datos))
 # Debemos crear un modelo para poder gestionar la base de datos (me refiero a insertar datos, actulizarlos
 # crear tablas...) El modelo se va a transformar en una tabla de SQL (esto va en el archivo models.py)
-# 
 
+# Estas funciones se desarrollaron con la intencion de hacer pruebas
+# por lo general estas las utilizamos para enviar interfaces al usuario
+# utilizando Tamplates (PLANTILLAS) archivos HTML que se encuentran en
+# la carpeta templetes (que creamos) la cual contiene archivos HTML
+# Asi (primero importamos render):
+
+def index(request):
+    # Dato de ejemplo:
+    title = 'Django Cours' # para mostrar este dato en el archivo html se lo pasamos como un tercer
+    # parametro a render en este caso.
+    # y ahora index tiene la variable title
+    return render(request, 'index.html', {
+        'title': title
+    })
+
+def about(request):
+    username = 'Richi'
+    return render(request, 'about.html', {
+        'username' : username
+    })
 
 def unaFuncion(request, usuario):
-    print(usuario)
     # Con HttpResponse Retornamos los datos de un modelo.
     # con %s() %(reemplace) podemos concatenear una variable en el dom
     return HttpResponse("<h2>Hello %s</h2>" % usuario)
 
 
-def about(request):
-    return HttpResponse('About')
+def projectos(request):
+    # Obtenemos el QuerySet y lo convetimos en una lista
+    # con values() obtenemos solamente los valores
+   # projects = list(tabla_uno_project.objects.values())
+    projects = tabla_uno_project.objects.all()
+    # Utilizamos el JsonResponse
+    # return JsonResponse(projects, safe=False)
+    return render(request, 'projects.html', {
+        'projects' : projects
+    })
 
-def index(request):
-    return HttpResponse("Index Page")
+
+# Consulta a la base de datos apartir de un parametro
+# def tareas(request, id):
+def tareas(request):
+    tasks = modelo_tareas_del_project.objects.all()
+    # task = modelo_tareas_del_project.objects.get(id=id)
+    # Esto es lo mismo de arriba pero con mas control en las busquedas sin resultado
+    # task = get_object_or_404(modelo_tareas_del_project, id = id)
+    # return HttpResponse('task: %s' % task.title)
+    return render(request, 'task.html', {
+        'tasks' : tasks
+    })
